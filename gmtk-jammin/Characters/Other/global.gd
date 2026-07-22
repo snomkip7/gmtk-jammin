1c6c51d67fe428b6aacae818428f3fdfb28e9956
snomkip7 <snomkip7@gmail.com>
2026-07-22 17:30:08 -0400

    direction changes
    timer and score there but non-functional

--- gmtk-jammin/Characters/Other/global.gd
+++ gmtk-jammin/Characters/Other/global.gd
@@ -1,3 +1,5 @@
 extends Node
 
 var player: CharacterBody3D
+var score = 0
+var time = 40
--- gmtk-jammin/Characters/Player/player.gd
+++ gmtk-jammin/Characters/Player/player.gd
@@ -2,9 +2,10 @@ extends CharacterBody3D
 
 var maxSpeed = 50
 var speed = 50
-var acceleration = 10 # how fast stuff moves
+var acceleration = 10 # how fast player changes movement
 var gravity = -10
-var dashVelocity = Vector2(100, 40)
+var dashVelocity = Vector2(100, 70)
+var direction = Vector3(0,0,-1) # ALWAYS NORMALIZED
 
 @onready var camera: Camera3D = $Camera
 @onready var sprite: Sprite3D = $PlayerSprite
@@ -16,16 +17,13 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	if Input.get_axis("backward", "forward") != 0 || Input.get_axis("left", "right") != 0:
 		# movement button is pressed
-		var direction = Vector3(Input.get_axis("left", "right"), 0, Input.get_axis("forward", "backward"))
+		direction = Vector3(Input.get_axis("left", "right"), 0, Input.get_axis("forward", "backward"))
 		velocity = velocity.move_toward(direction.normalized() * speed, acceleration)
 	else:
 		velocity = velocity.move_toward(Vector3.ZERO, acceleration)
 		
 	if Input.is_action_just_pressed("dash"):
 		print("ZOOMY TIME")
-		var direction = Vector3(Input.get_axis("left", "right"), 0, Input.get_axis("forward", "backward")).normalized()
-		if direction == Vector3.ZERO:
-			direction = Vector3(0,0,-1)
 		velocity += Vector3(dashVelocity.x * direction.x, dashVelocity.y, dashVelocity.x * direction.z)
 		
 	if !is_on_floor():
