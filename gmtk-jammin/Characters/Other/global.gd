fe3e890708427f7e9f04d731f16cab3739bee183
snomkip7 <snomkip7@gmail.com>
2026-07-22 16:51:00 -0400

    player global variable and movement

---
+++ gmtk-jammin/Characters/Other/global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var cameraRotation: Basis
+var player: CharacterBody3D
--- gmtk-jammin/Characters/Player/player.gd
+++ gmtk-jammin/Characters/Player/player.gd
@@ -1,3 +1,20 @@
 extends CharacterBody3D
 
-var speed = 50
+var speed = 50 
+
+func _ready():
+	global.cameraRotation = $Camera.global_basis
+	global.player = self
+	
+
+func _physics_process(delta: float) -> void:
+	if Input.get_axis("backward", "forward") != 0 || Input.get_axis("left", "right") != 0:
+		# movement button is pressed
+		var direction = Vector3(Input.get_axis("left", "right"), 0, Input.get_axis("forward", "backward"))
+		velocity = direction.normalized() * speed
+	else:
+		velocity = Vector3.ZERO
+	
+	move_and_slide()
+	
+	$PlayerSprite.rotation = $Camera.rotation
