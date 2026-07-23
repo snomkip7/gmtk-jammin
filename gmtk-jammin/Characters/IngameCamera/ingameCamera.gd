c9b3ba68c8c3dbd71b78905df4c8b9e622e985ff
snomkip7 <snomkip7@gmail.com>
2026-07-23 15:20:19 -0400

    no work

--- gmtk-jammin/Characters/IngameCamera/ingameCamera.gd
+++ gmtk-jammin/Characters/IngameCamera/ingameCamera.gd
@@ -13,4 +13,17 @@ func _physics_process(delta: float) -> void:
 func _on_shutter_timeout() -> void:
 	print("Timer ran out")
 	global.player.photo.texture = ImageTexture.create_from_image(get_texture().get_image())
+	print(get_texture().get_image().get_height())
+	print(get_texture().get_image().get_width())
+	print(get_texture().get_image().get_height()*get_texture().get_image().get_width())
 	#get_tree().current_scene.get_node("NPCGeneric").trigger(1)
+	var score: float = 0
+	for x: int in get_texture().get_image().get_width()/10:
+		for y: int in get_texture().get_image().get_height()/10:
+			if get_texture().get_image().get_pixel(x*5, y*5) == Color("9E80BC"):
+				score += 1
+			print(get_texture().get_image().get_pixel(x*5, y*5))
+			print(x*5, y*5)
+	print(score, " pixels")
+	score /= get_texture().get_image().get_height()*get_texture().get_image().get_width()
+	print(score, "%")
