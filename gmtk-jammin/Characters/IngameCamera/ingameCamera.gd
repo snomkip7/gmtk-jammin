38dba5b41333f8e1d97aa2b3541e3d3478fef41d
theEsteemed <CSEthegreat7297@gmail.com>
2026-07-23 13:12:50 -0400

    Camera text display + hitbox

--- gmtk-jammin/Characters/IngameCamera/ingameCamera.gd
+++ gmtk-jammin/Characters/IngameCamera/ingameCamera.gd
@@ -1,10 +1,14 @@
 extends SubViewport
 
 @onready var shutter: Timer = $Shutter
+@onready var timeDisplay: Label3D = $Camera/TimeDisplay
 
 func _physics_process(delta: float) -> void:
 	if(Input.is_action_just_released("interact")):
 		$Shutter.start()
+	
+	timeDisplay.text = str(shutter.time_left)
+	timeDisplay.rotation = Vector3(timeDisplay.rotation.x, (Vector2(global.player.camera.global_position.x, -global.player.camera.global_position.z) - Vector2(timeDisplay.global_position.x, -timeDisplay.global_position.z)).angle() + PI/2, timeDisplay.rotation.z)
 
 func _on_shutter_timeout() -> void:
 	print("Timer ran out")
