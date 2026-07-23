d0df99203c61abee236d03ffa64d253bf0ac6fc4
snomkip7 <snomkip7@gmail.com>
2026-07-23 14:20:07 -0400

    rounding moment

--- gmtk-jammin/Characters/Other/global.gd
+++ gmtk-jammin/Characters/Other/global.gd
@@ -9,12 +9,12 @@ func _process(delta: float) -> void:
 	if !paused:
 		time -= delta
 		if player != null && player.timer != null:
-			if roundi(time) % 60 < 10:
+			if ceili(time) % 60 < 10:
 				@warning_ignore("integer_division")
-				player.timer.text = str(roundi(time)/60)+":0"+str(roundi(time)%60)
+				player.timer.text = str(ceili(time)/60)+":0"+str(ceili(time)%60)
 			else:
 				@warning_ignore("integer_division")
-				player.timer.text = str(roundi(time)/60)+":"+str(roundi(time)%60)
+				player.timer.text = str(ceili(time)/60)+":"+str(ceili(time)%60)
 		if time <= 0:
 			endGame()
 			
