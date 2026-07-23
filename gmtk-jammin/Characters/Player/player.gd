e3949369d01df37cc0d98dd94e863655ae748470
snomkip7 <snomkip7@gmail.com>
2026-07-22 20:43:29 -0400

    sprite flag, dash change

--- gmtk-jammin/Characters/Player/player.gd
+++ gmtk-jammin/Characters/Player/player.gd
@@ -6,6 +6,7 @@ var acceleration = 10 # how fast player changes movement
 var gravity = -10
 var dashVelocity = Vector2(100, 70)
 var direction = Vector3(0,0,-1) # ALWAYS NORMALIZED
+var dashing = false
 
 @onready var camera: Camera3D = $Camera
 @onready var sprite: Sprite3D = $PlayerSprite
@@ -16,17 +17,18 @@ func _ready():
 	
 
 func _physics_process(delta: float) -> void:
-	if Input.get_axis("backward", "forward") != 0 || Input.get_axis("left", "right") != 0:
+	if !dashing && Input.get_axis("backward", "forward") != 0 || Input.get_axis("left", "right") != 0:
 		# movement button is pressed
 		direction = Vector3(Input.get_axis("left", "right"), 0, Input.get_axis("forward", "backward"))
 		velocity = velocity.move_toward(direction.normalized() * speed, acceleration)
 	else:
 		velocity = velocity.move_toward(Vector3.ZERO, acceleration)
 		
-	if Input.is_action_just_pressed("dash") && $DashCooldown.is_stopped():
+	if Input.is_action_just_pressed("dash") && !dashing:
 		print("ZOOMY TIME")
-		velocity += Vector3(dashVelocity.x * direction.x, dashVelocity.y, dashVelocity.x * direction.z)
+		velocity = Vector3(dashVelocity.x * direction.x, dashVelocity.y, dashVelocity.x * direction.z)
 		$DashCooldown.start()
+		dashing = true
 		
 	if !is_on_floor():
 		velocity.y += gravity
@@ -34,3 +36,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	$PlayerSprite.rotation = $Camera.rotation
+
+
+func dashEnd() -> void:
+	dashing = false
