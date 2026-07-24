fec32d29c2a53909233a44027397dbe2d5a6eca8
theEsteemed <CSEthegreat7297@gmail.com>
2026-07-23 21:06:57 -0400

    photos fading in and out

--- gmtk-jammin/Characters/Player/photo.gd
+++ gmtk-jammin/Characters/Player/photo.gd
@@ -5,17 +5,29 @@ var mod: bool = false
 
 @onready var deleteTimer: Timer = $DeleteTimer
 
+func _ready() -> void:
+	modulate.r = 0
+	modulate.g = 0
+	modulate.b = 0
+
 func _physics_process(delta: float) -> void:
 	
 	if(deleteTimer.is_stopped()):
 		deleteTimer.start()
 	
 	if(mod && texture != null):
-		modulate.a -= 10
+		modulate.a -= 0.1
+	
+	if(modulate.r <= 1 && modulate.g <= 1 && modulate.b <= 1):
+		modulate.r += 0.05
+		modulate.g += 0.05
+		modulate.b += 0.05
+	
+	if(modulate.a <= 0):
+		global.player.updatePhotos()
+		queue_free()
 	
 	rotation = lerpf(rotation, PI/20 * num, 0.5)
 
 func _on_delete_timer_timeout() -> void:
 	mod = true
-	global.player.updatePhotos()
-	queue_free()
