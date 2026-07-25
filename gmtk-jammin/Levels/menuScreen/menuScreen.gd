b1cd6088fa6deea3ba7d35c2f7d91bdd883887f9
snomkip7 <snomkip7@gmail.com>
2026-07-25 16:01:10 -0400

    space to start

--- gmtk-jammin/Levels/menuScreen/menuScreen.gd
+++ gmtk-jammin/Levels/menuScreen/menuScreen.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("continue"):
+		global.paused = false
+		get_tree().change_scene_to_file("res://Levels/testLevel.tscn")
+
 func startGame() -> void:
 	global.paused = false
 	get_tree().change_scene_to_file("res://Levels/testLevel.tscn")
