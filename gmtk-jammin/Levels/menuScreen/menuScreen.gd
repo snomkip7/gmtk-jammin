52248a7c9ea99d674527a5ac62fffa2a4c0520bf
snomkip7 <snomkip7@gmail.com>
2026-07-25 20:06:05 -0400

    level progress

--- gmtk-jammin/Levels/menuScreen/menuScreen.gd
+++ gmtk-jammin/Levels/menuScreen/menuScreen.gd
@@ -3,8 +3,11 @@ extends Node2D
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("continue"):
 		global.paused = false
+		get_tree().change_scene_to_file("res://Levels/Level.tscn")
+	if Input.is_action_just_pressed("interact"):
+		global.paused = false
 		get_tree().change_scene_to_file("res://Levels/testLevel.tscn")
 
 func startGame() -> void:
 	global.paused = false
-	get_tree().change_scene_to_file("res://Levels/testLevel.tscn")
+	get_tree().change_scene_to_file("res://Levels/Level.tscn")
